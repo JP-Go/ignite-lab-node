c7431de23f4c9b6bfe18fda8602cfb020f9255fc
João Pedro <jpedro1580@gmail.com>
2022-12-14 15:45:39 -0300

    Ignore

--- notification-service/src/application/entities/content.spec.ts
+++ notification-service/src/application/entities/content.spec.ts
@@ -3,7 +3,6 @@ import { Content } from './content';
 describe('Notification content test', () => {
   it('should be able to create a notification content', () => {
     const content = new Content('Você recebeu uma solicitação de amizade');
-
     expect(content).toBeTruthy();
   });
 
