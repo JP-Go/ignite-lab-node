810555724d50f8aba2b32d9f99a1974ae5ad01f2
João Pedro <jpedro1580@gmail.com>
2022-12-14 22:27:53 -0300

    Formatting

--- notification-service/test/repository/in-memory-notifications-repository.ts
+++ notification-service/test/repository/in-memory-notifications-repository.ts
@@ -3,6 +3,7 @@ import { Notification } from '../../src/application/entities/notification';
 
 export class InMemoryNotificationRepository implements NotificationRepository {
   public notifications: Notification[] = [];
+
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
