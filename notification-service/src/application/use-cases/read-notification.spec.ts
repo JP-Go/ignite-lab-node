3d914e1acf81a7dba8b8bf359426247cb4980a01
João Pedro <jpedro1580@gmail.com>
2022-12-16 00:50:22 -0300

    add and test read-notification

---
+++ notification-service/src/application/use-cases/read-notification.spec.ts
@@ -0,0 +1,31 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationRepository } from '@test/repository/in-memory-notifications-repository';
+import { ReadNotification } from './read-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Read notification test', () => {
+  it('should be able to read a notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const readNotification = new ReadNotification(notificationRepository);
+
+    const notification = makeNotification();
+    await notificationRepository.create(notification);
+
+    await readNotification.execute({ notificationId: notification.id });
+
+    expect(notificationRepository.notifications[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to read a non existing notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const readNotification = new ReadNotification(notificationRepository);
+
+    expect(() => {
+      return readNotification.execute({
+        notificationId: 'example-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
